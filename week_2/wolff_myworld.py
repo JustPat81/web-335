c716ddb3c2388e04eb2583539727ac721e4b61c8
Patrick Wolff <pat.wolff81@gmail.com>
2022-08-21 15:56:11 -0500

    Adding exercise 2.3

---
+++ week_2/wolff_myworld.py
@@ -0,0 +1,10 @@
+# Title: wolff_myworld.py
+# Author: Patrick Wolff
+# Date: 08 August 2022
+# Description: Python program to run myworld.py
+
+# Creating variable
+name = "Wolff"
+
+# Using the print() function to output variable
+print("You are now in " + name + "'s world")
