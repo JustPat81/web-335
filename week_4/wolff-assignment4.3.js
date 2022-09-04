e2db00c30c42322454a9375422d6b0aeb78cea73
Patrick Wolff <pat.wolff81@gmail.com>
2022-09-04 16:39:51 -0500

    Adding Week-4 Assignment

--- week_4/wolff-assignment4.3.js
+++ week_4/wolff-assignment4.3.js
@@ -11,4 +11,21 @@
 db.users.find( {} )
 
 // Query to find a user by email address
-db.users.find({"jbach@me.com"})
+db.users.find(
+    { "email": "jbach@me.com" }
+)
+
+// Query to find user by last name
+db.users.find(
+    { "lastName": "Mozart" }
+)
+
+// Query to find user by first name
+db.users.find(
+    { "firstName": "Richard" }
+)
+
+// Query to find user by employeeId
+db.users.find(
+    { "employeeId": "1010" }
+)
