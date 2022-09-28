d065be125bf6920ef6b7122f4016412005055de4
Patrick Wolff <pat.wolff81@gmail.com>
2022-09-27 19:32:45 -0500

    Delete whatabook directory

--- whatabook/brainybadgers-whatabook.js
+++
@@ -1,8 +0,0 @@
-/* =========================================
-* Title: brainybadgers-whatabook.js
-* Authors: Patrick Wolff, Kailee Stephens, Anita Taylor
-* Date: 27 September 2022
-* Description: Queries for WhatABook project
-* ==========================================
-*/
-
